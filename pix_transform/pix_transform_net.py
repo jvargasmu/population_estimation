bug fix with input scaling

pix_transform/pix_transform_net.py
@@ -246,9 +246,9 @@ class PixScaleNet(nn.Module):
     def perform_scale_inputs(self, data, name):
         if name not in self.datanames:
             self.calculate_mean_input_scale()
-            return (data-self.mean_in_bias) / self.mean_in_scale #+ self.mean_in_bias
+            return (data - self.mean_in_bias) / self.mean_in_scale #+ self.mean_in_bias
         else:
-            return data*self.in_scale[name] + self.in_bias[name]
+            return (data - self.in_bias[name]) /self.in_scale[name]
 
 
     def calculate_mean_input_scale(self):