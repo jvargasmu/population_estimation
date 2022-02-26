e06159300187d76c75567ba726c328f904a6cecd
nandometzger <metzgern@ethz.ch>
2022-02-26 15:01:05 +0100

    comment uimportant import

--- pix_transform/evaluation.py
+++ pix_transform/evaluation.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from torch._C import AliasDb
+#from torch._C import AliasDb
 logging.basicConfig( format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 import numpy as np
