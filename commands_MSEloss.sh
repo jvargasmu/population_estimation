5f6bcf68c8d7976a1c7441a52a3135127e8c49a2
John Vargas <jvargasmu@gmail.com>
2023-02-08 16:27:09 +0100

    remove unused files

--- commands_MSEloss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-superpixel_disagg_model.py -train tza --validation_fold 0 --name runame-0
-superpixel_disagg_model.py -train tza --validation_fold 1 --name runame-1
-superpixel_disagg_model.py -train tza --validation_fold 2 --name runame-2
-superpixel_disagg_model.py -train tza --validation_fold 3 --name runame-3
-superpixel_disagg_model.py -train tza --validation_fold 4 --name runame-4
-superpixel_disagg_model.py -train tza -e5f runame-1,runame-2,runame-3,runame-4 -e5fmt best_r2
